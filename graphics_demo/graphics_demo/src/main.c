697d069c00ab421fce2a3337d0c818fa9e946c4b
DonaghOB <donaghobriain5@gmail.com>
2026-02-25 12:31:29 +0000

    Branch retest

--- graphics_demo/graphics_demo/src/main.c
+++ graphics_demo/graphics_demo/src/main.c
@@ -5,7 +5,7 @@ void initSysTick(void);
 void SysTick_Handler(void);
 void delay(volatile uint32_t dly);
 void setupIO();
-int isInside(uint6_t x1, uint16_t y1, uint16_t w, uint16_t h, uint16_t px, uint16_t py);
+int isInside(uint16_t x1, uint16_t y1, uint16_t w, uint16_t h, uint16_t px, uint16_t py);
 void enablePullUp(GPIO_TypeDef *Port, uint32_t BitNumber);
 void pinMode(GPIO_TypeDef *Port, uint32_t BitNumber, uint32_t Mode);
 
